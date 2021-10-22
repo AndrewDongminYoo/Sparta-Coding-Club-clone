faec2b09ecc64fc4d266988d1a376c1d493f5ee2
AndrewDongminYoo <ydm2790@gmail.com>
2021-10-22 17:57:08 +0900

    application.py

--- application.py
+++ application.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+import os
 
-app = Flask(__name__)
+application = Flask(__name__, static_folder='static', template_folder='templates')
+client = MongoClient('localhost', port=27017)
+db = client.get_database('Lecture')
+if application.env == 'development':
+    os.popen('mongod')
 
 
-@app.route('/')
+@application.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@application.route('/enrolment')
+def enrolment():
+    return render_template('EnrollPage.html')
+
+
+@application.route('/lecture')
+def show_video():
+    return render_template('LecturePage.html')
+
+
+@application.route('/roadmap')
+def show_roadmap():
+    return render_template('RoadmapPage.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    application.run()
